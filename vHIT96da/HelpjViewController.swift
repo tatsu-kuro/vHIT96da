f3a51ef714120a84f746ff51b3ef77ed949e87e5
tatsuaki <tatsuakikuroda@me.com>
2023-03-02 22:02:21 +0900

    helpPage

--- vHIT96da/HelpjViewController.swift
+++ vHIT96da/HelpjViewController.swift
@@ -8,107 +8,64 @@
 
 import UIKit
 
-class HelpjViewController: UIViewController{
+class HelpjViewController: UIViewController {
+//    let someFunctions = myFunctions()
+    let iroiro = myFunctions(albumName: "vHIT_VOG")
+  
     var calcMode:Int?
-    var jap_eng:Int=0
-    @IBOutlet weak var helpView: UIImageView!
+
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var langButton: UIButton!
-    var currentImageName:String!
-    func setHelpImage(){
-        let top=CGFloat(UserDefaults.standard.float(forKey: "top"))
-        if jap_eng==1{
-            if calcMode != 2{
-                currentImageName="vHITen"
-            }else{
-                currentImageName="VOGen"
-            }
-        }else{
-            if calcMode != 2{
-                currentImageName="vHITja"
-            }else{
-                currentImageName="VOGja"
-            }
-        }
-        helpView.image = UIImage(named:currentImageName)!
-        let image:UIImage = UIImage(named:currentImageName)!
-        // 画像の縦横サイズを取得
-        let imgWidth:CGFloat = image.size.width
-        let imgHeight:CGFloat = image.size.height
-        // 画像サイズをスクリーン幅に合わせる
-        let scale:CGFloat = imgHeight / imgWidth
-        helpView.frame=CGRect(x:0,y:top,width:view.bounds.width,height: view.bounds.width*scale)
-    }
- 
     
-    @IBAction func langChan(_ sender: Any) {
-        if jap_eng==0{
-            jap_eng=1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let top=CGFloat(UserDefaults.standard.float(forKey: "top"))
+        let bottom=CGFloat(UserDefaults.standard.float(forKey: "bottom"))
+        let ww=view.bounds.width
+        let wh=view.bounds.height - (top+bottom)
+        let sp:CGFloat=5
+        let bw=(ww-sp*10)/7//ボタン幅
+        let bh=bw
+        let by=view.bounds.height-bottom-2*sp-bh
+
+        // 画面サイズ取得
+        scrollView.frame = CGRect(x:0,y:top,width: ww,height: wh)
+        iroiro.setButtonProperty(exitButton,x:bw*6+sp*8,y:by,w:bw,h:bh,UIColor.darkGray)
+
+        var img = UIImage(named:"vHITen")!
+    
+        if Locale.preferredLanguages.first!.contains("ja"){
+            if calcMode != 2{
+                img = UIImage(named:"vHITja")!
+            }else{
+                img = UIImage(named: "VOGja")!// currentImageName="VOGen"
+            }
         }else{
-            jap_eng=0
+            if calcMode != 2{
+                img = UIImage(named:"vHITja")!//currentImageName="vHITja"
+            }else{
+                img = UIImage(named:"VOGja")!//currentImageName="VOGja"
+            }
         }
-        setHelpImage()
-        UserDefaults.standard.set(0,forKey:"currentHelpY")
+        
+        // 画像のサイズ
+        let imgW = img.size.width
+        let imgH = img.size.height
+        let image = img.resize(size: CGSize(width:ww, height:ww*imgH/imgW))
+        // UIImageView 初期化
+        let imageView = UIImageView(image: image)
+        // UIScrollViewに追加
+        scrollView.addSubview(imageView)
+        // UIScrollViewの大きさを画像サイズに設定
+        scrollView.contentSize = CGSize(width: ww, height: ww*imgH/imgW)
+        // スクロールの跳ね返り無し
+        scrollView.bounces = true
     }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("viewdidload")
-        if Locale.preferredLanguages.first!.contains("ja"){
-            jap_eng=1//langChan()で表示するので０でなくて１
-        }else{
-            jap_eng=0
-        }
-        langChan(0)//contains setHelpImage()
-        UserDefaults.standard.set(0,forKey:"currentHelpY")
-        setButtons()
-    }
-
-    func getUserDefaultFloat(str:String,ret:Float) -> Float{
-        if (UserDefaults.standard.object(forKey: str) != nil){
-            return UserDefaults.standard.float(forKey: str)
-        }else{//keyが設定してなければretをセット
-            UserDefaults.standard.set(ret, forKey: str)
-            return ret
-        }
-    }
-  
-    var posYlast:CGFloat=0
-    @IBAction func panGestuer(_ sender: UIPanGestureRecognizer) {
-        let move=sender.translation(in: self.view)
-        let top=CGFloat(UserDefaults.standard.float(forKey: "top"))
-        let height=helpView.frame.size.height
-        let exitY=exitButton.frame.minY
-        if height < exitY-top{
-            return
-        }
-        if sender.state == .began {
-            posYlast=helpView.frame.origin.y
-        }else if sender.state == .changed {
-            let temp=posYlast+move.y
-            if temp>top{
-                helpView.frame.origin.y=top
-            }else if temp+height < exitY{//} && height < exitY-top{
-                helpView.frame.origin.y = -height+exitY
-            }else{
-                helpView.frame.origin.y=temp
-            }
-            print("helpview:",helpView.frame.origin.y,move.y)
-         }else if sender.state == .ended{
-        }
-    }
-
-    func setButtons(){
-        let bottomPadding=CGFloat(UserDefaults.standard.float(forKey: "bottom"))
-        let sp:CGFloat=5
-        let butw=(view.bounds.width-sp*7)/4
-        let buth=butw/2
-        let buty=view.bounds.height-sp-buth-bottomPadding
-        langButton.frame=CGRect(x:2*sp,y:buty,width:butw,height: buth)
-        exitButton.frame=CGRect(x:butw*3+5*sp,y:buty,width:butw,height: buth)
-        langButton.layer.cornerRadius = 5
-        exitButton.layer.cornerRadius = 5
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
     }
 }
