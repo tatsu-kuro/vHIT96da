1b54293b3d1ac13d9856b0181665599a7ef68e21
tatsuaki <tatsuakikuroda@me.com>
2023-04-02 17:37:41 +0900

    完成　新幹線の中（鳥栖あたり）

--- vHIT96da/KeySettingViewController.swift
+++ vHIT96da/KeySettingViewController.swift
@@ -73,21 +73,43 @@ class KeySettingViewController: UIViewController,MFMailComposeViewControllerDele
         let ww=view.bounds.width
         let bottom=CGFloat( UserDefaults.standard.float(forKey: "bottom"))
         let top=CGFloat(UserDefaults.standard.float(forKey: "top"))
+        print("top:",top)
 //        let bottom:CGFloat=10
 //        let top:CGFloat=10
         let sp:CGFloat=5
-        let bw:CGFloat=55
-        let bh:CGFloat=25
+        let bw:CGFloat=60
+        let bh:CGFloat=30
    
-        statementLabel.frame=CGRect(x:0,y:top,width: ww,height: wh-top-bottom-sp-bh)
-        statementLabel.text="医師、理学療法士が利用するvHITアプリです。自作可能なiPhone固定ゴーグルとiPhoneでvHITが行えます。日本国内で研究用として承認されています。\n使用に際しては、参加者または未成年の場合はその親または保護者から同意を得る必要があります。その同意には、（a）研究の性質、目的および期間、（b）手順、参加者に対するリスクおよび利益、（c）データの機密保持および取り扱い（第三者との共有を含む）に関する情報、（d）参加者からの質問に対する連絡先、および（e）撤回手続が含まれなければなりません。このアプリを使用するためには、設定ページから登録ページに行き、キーを設定する必要があります。氏名、メールアドレス、所属を記載してキーを申請して下さい。キーを設定すると全ての機能が利用可能となります。"
+        statementLabel.frame=CGRect(x:10,y:top+20,width: ww-20,height: wh-top-bottom-20-sp-bh*2)
+        if Locale.preferredLanguages.first!.contains("ja"){
+            statementLabel.text="医師、理学療法士が利用するvHITアプリです。自作可能なiPhone固定ゴーグルとiPhoneでvHITが行えます。日本国内で研究用として承認されています。\n使用に際しては、参加者または未成年の場合はその親または保護者から同意を得る必要があります。その同意には、\n（a）研究の性質、目的および期間\n（b）手順、参加者に対するリスクおよび利益\n（c）データの機密保持および取り扱い（第三者との共有を含む）に関する情報\n（d）参加者からの質問に対する連絡先\n（e）撤回手続\nが含まれなければなりません。"
+        }else{
+            statementLabel.text="This application is used by physicians and physical therapists. With this app, vHIT can be perfomed with goggles that can be made by the user and an iPhone. This vHIT has been approved for reserch use in Japnan.\nBefore using this application obtain consent from participants or, in the case of minors, their parent or guardian. Such consent must include \n(a) nature, purpose\nduration of the research\n(b) procedures, risks, and benefits to the participant\n(c) information about confidentiality and handling of data (including any sharing with third parties)\na point of contact for participant questions\n(e) the withdrawal process."
+        }
         nextButton.frame=CGRect(x:ww/2-bw/2,y:wh-bottom-sp-bh,width: bw,height: bh)
+        nextButton.layer.cornerRadius=5
+        exitButton.frame=CGRect(x:ww/2-bw,y:wh-bottom-sp-bh*2,width: bw*2,height: bh*2)
         exitButton.layer.cornerRadius=5
-        statementLabel.isHidden=true
+        keyStatusText.frame=CGRect(x:40,y:wh-bottom-sp-bh*2-60,width:ww-80,height: 34)
+        exitButton.isHidden=true
+//        statementLabel.isHidden=true
         mailAddressText.keyboardType = .emailAddress
         keyTextSet()
     }
     
+    @IBAction func onNextButton(_ sender: Any) {
+        statementLabel.isHidden=true
+        nextButton.isHidden=true
+//        exitButton.isHidden=false
+    }
+    /*
+     var mess="This application is used by physicians and physical therapists. With this app, vHIT can be perfomed with goggles that can be made by the user and an iPhone. This vHIT has been approved for reserch use in Japnan.\nBefore using this application obtain consent from participants or, in the case of minors, their parent or guardian. Such consent must include the (a) nature, purpose, and duration of the research; (b) procedures, risks, and benefits to the participant; (c) information about confidentiality and handling of data (including any sharing with third parties); (d) a point of contact for participant questions; and (e) the withdrawal process.\nTo use this app, you must go to the registration page from the Settings page and set up a key. Please apply for a key with your name, email address, and affiliation. Once the key is set, all functions will be available."
+//        if Locale.preferredLanguages.first!.contains("ja"){
+//            title="vHITアプリ"
+//            okText="OK"
+//            mess="医師、理学療法士が利用するvHITアプリです。自作可能なiPhone固定ゴーグルとiPhoneでvHITが行えます。日本国内で研究用として承認されています。\n使用に際しては、参加者または未成年の場合はその親または保護者から同意を得る必要があります。その同意には、（a）研究の性質、目的および期間、（b）手順、参加者に対するリスクおよび利益、（c）データの機密保持および取り扱い（第三者との共有を含む）に関する情報、（d）参加者からの質問に対する連絡先、および（e）撤回手続が含まれなければなりません。このアプリを使用するためには、設定ページから登録ページに行き、キーを設定する必要があります。氏名、メールアドレス、所属を記載してキーを申請して下さい。キーを設定すると全ての機能が利用可能となります。"
+
+     */
     @IBAction func onSetKeyButton(_ sender: Any) {
         keyText.endEditing(true)
         mailAddressText.endEditing(true)
@@ -95,10 +117,21 @@ class KeySettingViewController: UIViewController,MFMailComposeViewControllerDele
         print("hash20:",str)
         if str == keyText.text{
             UserDefaults.standard.set(true,forKey: "keyGet")
+            exitButton.isHidden=false
         }
         keyTextSet()
     }
   
+    @IBAction func panGestureRec(_ sender: UIPanGestureRecognizer) {
+   
+        if sender.state == .ended{
+            let move = sender.translation(in: self.view)
+            if ( move.y > 20)
+            {
+                mailAddressText.endEditing(true)
+            }
+        }
+    }
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {//errorの時に通る
         
         switch result {
